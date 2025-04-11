c175dcf063b5135dd1cc7d9bdcefd9ed4f22e7da
Ahmed <ahmed.hussein@zarcony.dev>
2025-04-11 18:09:52 +0200

    Handle pagination in absence screen

--- lib/ui/absence/widgets/absence_screen.dart
+++ lib/ui/absence/widgets/absence_screen.dart
@@ -1,6 +1,7 @@
 import 'package:absence_manager/ui/absence/bloc/absence_bloc.dart';
+import 'package:absence_manager/ui/absence/bloc/absence_event.dart';
 import 'package:absence_manager/ui/absence/bloc/absence_state.dart';
-import 'package:absence_manager/ui/absence/widgets/absence_tile.dart';
+import 'package:absence_manager/ui/absence/widgets/absences_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,29 +11,30 @@ class AbsencesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Absences")),
+      appBar: AppBar(title: Text("Absences")),
       body: BlocBuilder<AbsencesBloc, AbsencesState>(
         builder: (context, state) {
           if (state is AbsencesLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is AbsencesLoaded) {
-            if (state.absences.isEmpty) {
-              return const Center(child: Text("No absences found."));
-            }
-            return ListView.builder(
-              itemCount: state.absences.length,
-              itemBuilder: (context, index) {
-                final absenceWithMember = state.absences[index];
-                return AbsenceTile(data: absenceWithMember);
-              },
-            );
+            return Center(child: CircularProgressIndicator());
           } else if (state is AbsencesError) {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Center(child: Text("Error: ${state.message}", textAlign: TextAlign.center)),
             );
+          } else if (state is AbsencesLoaded) {
+            return AbsencesList(
+              absenceWithMember: state.absences,
+              hasMore: state.hasMore,
+              onLoadMore: () {
+                if (state.hasMore) {
+                  context.read<AbsencesBloc>().add(
+                    LoadMoreAbsences(offset: state.absences.length, limit: 10),
+                  );
+                }
+              },
+            );
           }
-          return const SizedBox.shrink(); // AbsencesInitial
+          return Center(child: Text("No data"));
         },
       ),
     );
