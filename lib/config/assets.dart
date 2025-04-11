38fca9c5c1fd169501ce385515ec2aacfa955537
Ahmed <ahmed.hussein@zarcony.dev>
2025-04-11 15:50:18 +0200

    Implement use case to return absences with associated member data and Register it in service locator

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,7 +1,3 @@
-// Copyright 2024 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 abstract final class Assets {
   static const absences = 'assets/json/absences.json';
   static const members = 'assets/json/members.json';
