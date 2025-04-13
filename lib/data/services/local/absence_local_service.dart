1a617f14f45dbb36f7240d455bce2408bd192525
Ahmed <ahmed.hussein@zarcony.dev>
2025-04-13 20:12:35 +0200

    store absences and members by ID in Hive to prevent duplication and enable updates

--- lib/data/services/local/absence_local_service.dart
+++ lib/data/services/local/absence_local_service.dart
@@ -4,13 +4,18 @@ import 'package:hive/hive.dart';
 class AbsenceLocalService {
   static const String _boxName = 'absence_cache';
 
+  /// Save absences by using their unique ID as Hive key (avoids duplication)
   Future<void> saveAbsences(List<AbsenceCacheModel> absences) async {
     final box = await Hive.openBox<AbsenceCacheModel>(_boxName);
-    await box.clear();
-    await box.addAll(absences);
+    for (final absence in absences) {
+      if (absence.id != null) {
+        await box.put(absence.id, absence);
+      }
+    }
     await box.close();
   }
 
+  /// Get all cached absences (unsorted)
   Future<List<AbsenceCacheModel>> getCachedAbsences() async {
     final box = await Hive.openBox<AbsenceCacheModel>(_boxName);
     final list = box.values.toList();
