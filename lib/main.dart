bebd3c9cbe44fa1d49f9a613091ae98e31aa379b
Ahmed <ahmed.hussein@zarcony.dev>
2025-04-13 14:34:47 +0200

    setup Hive and define cache models for absence and member

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:absence_manager/config/service_locator.dart';
+import 'package:absence_manager/data/services/local/model/absence/absence_cache_model.dart';
+import 'package:absence_manager/data/services/local/model/member/member_cache_model.dart';
 import 'package:absence_manager/ui/absence/bloc/absence_bloc.dart';
 import 'package:absence_manager/ui/absence/bloc/absence_event.dart';
 import 'package:absence_manager/ui/absence/widgets/absence_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(AbsenceCacheModelAdapter());
+  Hive.registerAdapter(MemberCacheModelAdapter());
 
   await setupLocator();
 
